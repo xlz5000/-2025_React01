528359d1385fddad8fb9304f9adfd772ed4b8648
JangDabin <xlzdscs5@gmail.com>
2025-05-14 09:55:28 +0900

    05-14-2

---
+++ src/pages/step05/items.jsx
@@ -0,0 +1,22 @@
+export default funtcion Items(props){
+
+    return(
+        <>
+            <ul>
+                <li>이름:</li>
+                <li>내용:</li>
+            
+            
+            
+            </ul>
+        
+        
+        
+        
+        
+        </>
+
+
+
+    );
+}
